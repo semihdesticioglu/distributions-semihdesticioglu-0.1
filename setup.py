1ec76f43f7986d1269841d82c606ccdecc4add08
semihnykv <55434804+semihnykv@users.noreply.github.com>
2020-12-06 22:22:14 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_semihdesticioglu',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_semihdesticioglu'],
+      author = "Semih Desticioglu",
+      author_email = "desticioglu@gmail.com" ,
+      zip_safe=False)
